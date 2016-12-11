03d03ed05ba4957d5737fec1a12e1160c107c1b0
davelasquez12 <davelasquez12@gmail.com>
2016-12-10 23:33:34 -0600

    Added favorite functionality

--- music/urls.py
+++ music/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
     url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
 
     # music/<album id>/favorite/
-   # url(r'^(?P<album_id>[0-9]+)/favorite/$', views.favorite, name='favorite'),
+    url(r'^(?P<album_id>[0-9]+)/favorite/$', views.favorite, name='favorite'),
 ]
